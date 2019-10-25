2a27503ef3c275b490a8f451c11b6794872ab6c1
Nathan noye <32617224+NathanNoye@users.noreply.github.com>
2019-10-25 14:18:09 -0300

    Added crypto class - still WIP

---
+++ agile.core.js
@@ -0,0 +1,115 @@
+var Agile = {} || Agile;
+Agile.core = {} || Agile.core;
+Agile.components = {} || Agile.components;
+Agile.observable = {} || Agile.observable;
+Agile.fx = {} || Agile.fx;
+Agile.emit = {} || Agile.emit;
+Agile.crypto = {} || Agile.crypto;
+
+Agile.core = {
+    VM_LIST: [],
+    /**
+        * @param {rootEl} Root HTML element
+        * @param {vm} View Model you're attaching to
+        */
+       bind: function (rootEl, vm) {
+        [].slice.call(rootEl.querySelectorAll('[data-bind]')).forEach(el => {
+            var events = el.getAttribute('data-bind').split(',');
+            events.forEach(event => {
+                var _eventType = event.split(':')[0].replace(' ', '').toLowerCase();
+                var fn = event.split(':')[1].replace(' ', '').toString();
+                if (_eventType === 'text') {
+                    vm[fn] = new Agile.observable.text(el);
+                }
+                if (_eventType === 'css') {
+                    vm[fn] = new Agile.observable.class(el, fn);
+                }
+                el.addEventListener(_eventType, function (e) {
+                    e.preventDefault();
+                    try {
+                        vm[fn](e, el);
+                    } catch (err) {
+                        console.error(`There's an error with the ${fn} function `, err)
+                    }
+                });
+                el.removeAttribute('data-bind');
+            })
+        });
+        rootEl.removeAttribute('data-props');
+    },
+    /**
+     * @param {JSON} config.where beforebegin, afterbegin, beforeend, afterend
+     * @param {JSON} config.el the element to insert the HTML
+     * @param {JSON} config.html The actual HTML
+     */
+    insertHTML: function (config) {
+        config.where = config.where || 'beforeend';
+        config.el.insertAdjacentHTML(config.where, config.html);
+    },
+    /**
+     * @description Checks if the user browser is mobile
+     */
+    isMobile: function () {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    },
+    /**
+     * @description Checks if the website is being viewed via a PWA in standalone mode
+     */
+    isPWA: function () {
+        return (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone);
+    },
+    /**
+     * @description Used to initialize all the components in the Agile.components group
+     */
+    initializeComponents: function () {
+        document.querySelectorAll('[data-component]').forEach(key => {
+            Agile.core.VM_LIST.push(new Agile.components[key.getAttribute('data-component')](key));
+        });
+    },
+    /**
+     * @param {JSON} config object: before(), always(), method, url, parms |=====| REQUIRED
+     * @param {fn} before() - called at the very start of the function |=====| OPTIONAL
+     * @param {fn}	after() - always called after success() and fail() |=====| OPTIONAL
+     * @param {HTTP method | String} method GET, POST, PUT, DELETE, ... Default is POST |=====| OPTIONAL
+     * @param {URL | String} url URL of the end. Could be a local file or a URL |=====| REQUIRED
+     * @param {JSON} parms parameters, like your post parameters |=====| OPTIONAL
+     * @memberof Agile.core namespace
+     * @returns {Promise}
+    */
+    ajax: function (config) {
+        return new Promise(function (resolve, reject) {
+            config.method = config.method.toUpperCase() || "POST";
+            typeof config.before === "function" ? config.before() : null;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                    if (xmlhttp.status == 200) {
+                        resolve(xmlhttp)
+                    }
+                    else {
+                        reject(xmlhttp)
+                    }
+                    typeof config.after === "function" ? config.after(xmlhttp) : null;
+                }
+            };
+            xmlhttp.open(config.method, config.url);
+
+            if (config.parms) {
+                xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                let parmString = "";
+                for (var key in config.parms) {
+                    if (config.parms.hasOwnProperty(key)) {
+                        parmString += key + "=" + config.parms[key] + "&";
+                    }
+                }
+                xmlhttp.send(parmString.substr(0, parmString.length - 1))
+            } else {
+                xmlhttp.send();
+            }
+        });
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    Agile.core.initializeComponents();
+});
